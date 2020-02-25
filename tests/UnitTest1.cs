7fa903a4466dc64763cf87c1c3c868ffbd063fdb
Wyatt Byroade <wabyroade8918@eagle.fgcu.edu>
2020-02-24 22:20:04 -0500

    Initial commit of working unit test structure

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+using src;
+
+namespace tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void ItExists()
+        {
+            var fizzBuzz = new FizzBuzz();
+        }
+    }
+}

eb3eb03c1500482c814fa0767adddf85517a6b40
Wyatt Byroade <wabyroade8918@eagle.fgcu.edu>
2020-02-24 23:00:21 -0500

    Add coverage of main return values

--- src/FizzBuzz.cs
+++ src/FizzBuzz.cs
@@ -6,7 +6,20 @@ namespace src
     {
         public string ReturnValue(int inputValue)
         {
-            return "Fizz";
+            string responseString = "";
+            if (inputValue % 3 == 0)
+            {
+                responseString += "Fizz";
+            }
+            if (inputValue % 5 == 0)
+            {
+                responseString += "Buzz";
+            }
+            if (responseString == "")
+            {
+                return inputValue.ToString();
+            }
+            return responseString;
         }
     }
 }
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -24,8 +24,37 @@ namespace tests
         [Fact]
         public void ReturnFizzOn3()
         {
+        //Given
             string response = _fizzBuzz.ReturnValue(3); 
+        //Then
             Assert.Equal("Fizz", response);
+        }
+
+        [Fact]
+        public void ReturnBuzzOn5()
+        {
+        //Given
+            string response = _fizzBuzz.ReturnValue(5);
+        //Then
+            Assert.Equal("Buzz",response);
+        }
+
+        [Fact]
+        public void ReturnNumberOn1()
+        {
+        //Given
+            string response = _fizzBuzz.ReturnValue(1);
+        //Then
+            Assert.Equal("1",response);
+        }
+
+        [Fact]
+        public void ReturnFizzBuzzOn15()
+        {
+        //Given
+            string response = _fizzBuzz.ReturnValue(15);
+        //Then
+            Assert.Equal("FizzBuzz",response);
         }
     }
 }
